99c5094e465c88b57a9a507ce0e237eee17ecf74
aawsolutions <git@aawsolutions.com>
2010-11-28 14:17:08 -0600

    accomodated the new logo with slogan, tons of minor visual changes (image changes not tracked here)

--- sermonaudio/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from sermonaudio.models import *
-
-class SermonAdmin(admin.ModelAdmin):
-    list_filter = ('delivered',)
-admin.site.register(Sermon, SermonAdmin)
