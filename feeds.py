2d85bb66f6816074a1cc8f8efe8a5f136559e6f6
aawsolutions <git@aawsolutions.com>
2010-12-19 20:39:35 -0600

    forgot to limit the rss feed to be less-than-or-equal-to today...

--- feeds.py
+++ feeds.py
@@ -5,14 +5,15 @@ from bslcks.bslctags.templatetags import mytags
 from bslcks.bslctags.templatetags import process_flat
 from basic.inlines.templatetags import inlines
 
+from datetime import datetime
 
 class LatestNewsFeed(Feed):
     title = "Beautiful Savior Lutheran Church Current News"
-    link = "http://www.bslcks.info/news/"
+    link = "http://www.bslcks.org/news/"
     description = "10 most recent storie from the Beautiful Savior website"
 
     def items(self):
-        return Post.objects.filter(status=2).order_by('-publish')[:10]
+        return Post.objects.filter(status=2).filter(publish__lte=datetime.now()).order_by('-publish')[:10]
 
     def item_title(self, item):
         return item.title
