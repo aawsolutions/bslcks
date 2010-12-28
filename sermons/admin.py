e3e4e04a4bc431d1f6495330aa98d765ecafdae0
aawsolutions <git@aawsolutions.com>
2010-12-28 16:55:49 -0600

    more fixits post MEDIA-STATIC debacle

--- sermons/admin.py
+++ sermons/admin.py
@@ -3,4 +3,5 @@ from sermons.models import *
 
 class SermonAdmin(admin.ModelAdmin):
     list_filter = ('delivered', 'category',)
+    ordering = ['-delivered',]
 admin.site.register(Sermon, SermonAdmin)
